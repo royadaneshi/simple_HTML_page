3f5af5db329a789936a61e48390057406d85840a
your_username <royadaneshi2001@gmail.com>
2023-07-15 11:35:32 +0330

    finish

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function read_more_btn() {
 //close
   if (sidebar.classList.contains("open")) {
     sidebar.classList.remove("open");
-    firstRec.style.width = "calc(100% - 21vw)";
-    document.getElementById("mySidebar").style.width = "20vw";
+    firstRec.style.width = "calc(100% - 23vw)";
+    document.getElementById("mySidebar").style.width = "22vw";
 
     const elements = document.getElementsByClassName("items");
     for (var i = 0; i < elements.length; i++) {
@@ -19,6 +19,9 @@ function read_more_btn() {
     document.getElementById("title").style.display = "block";
     document.getElementById("time").style.paddingRight="7vw";
 
+    var bell = document.getElementsByClassName("bell")[0];
+    bell.style.fontSize = "7vh";
+    // document.getElementByClassName("first_rec").style.
 
   } else {
     sidebar.classList.add("open");
@@ -79,25 +82,31 @@ function read_more_btn() {
     document.getElementById("title").style.display = "none";
     document.getElementById("time").style.paddingRight="1vw";
 
-
+    var bell = document.getElementsByClassName("bell")[0];
+    bell.style.fontSize = "4vh";
   }
 }
 
 function calander() {
   clock() 
+  setInterval(clock, 1000);
   var currentDate =new Date(); //"2023-07-03"
-  var currentDay = currentDate.getDay()+1; // 0 (شنبه)  6 (جمعه)
-  var firstDayOffset = currentDay > -1 ? currentDay - 1 : 7;
-  
-  var firstDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (firstDayOffset+1));
 
+  if (currentDate.getDay()==6){
+    var currentDay = 0;
+  }else{
+    var currentDay = currentDate.getDay()+1; // 0 (شنبه)  6 (جمعه)
+
+  }
+  var firstDayOffset = currentDay > -1 ? currentDay - 1 : 7;
+  var firstDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (firstDayOffset+1));
   var second = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()- (firstDayOffset));
   var third = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (firstDayOffset-1));
   var fourth =  new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (firstDayOffset-2));
   var fifth = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (firstDayOffset-3));
   var sixth = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (firstDayOffset-4));
-
   var lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - (firstDayOffset-5));
+
 
   document.getElementById("one").textContent = String(firstDay.getDate());
   document.getElementById("two").textContent = String(second.getDate());
@@ -109,25 +118,25 @@ function calander() {
 
   switch (currentDay) {
     case 0:
-      document.getElementById("one").style.backgroundColor = "#b311b3";
+      document.getElementById("one").style.backgroundColor = "#860f86";
       break;
     case 1:
-      document.getElementById("two").style.backgroundColor = "#b311b3";
+      document.getElementById("two").style.backgroundColor = "#860f86";
       break;
     case 2:
-    document.getElementById("three").style.backgroundColor = "#b311b3";
+    document.getElementById("three").style.backgroundColor = "#860f86";
     break;
     case 3:
-      document.getElementById("four").style.backgroundColor = "#b311b3";
+      document.getElementById("four").style.backgroundColor = "#860f86";
       break;
     case 4:
-      document.getElementById("five").style.backgroundColor = "#b311b3";
+      document.getElementById("five").style.backgroundColor = "#860f86";
       break;
     case 5:
-      document.getElementById("six").style.backgroundColor = "#b311b3";
+      document.getElementById("six").style.backgroundColor = "#860f86";
       break;
     case 6:
-    document.getElementById("seven").style.backgroundColor = "#b311b3";
+    document.getElementById("seven").style.backgroundColor = "#860f86";
     break;
 
     default:
